749f675292bb7820fe28ee3b3e5cdccada862a78
Andrew McConachie <andrew@depht.com>
2020-11-17 14:53:29 +0000

    working on consolidation

--- process_skarf.py
+++ process_skarf.py
@@ -7,11 +7,14 @@ import argparse
 import urllib.parse
 
 class FediServer():
-  def __init__(self, domain, ts):
+  def __init__(self, domain, first_seen, last_seen=None):
     self.domain = domain
-    self.first_seen = ts
-    self.last_seen = ts
     self.hits = 1
+    self.first_seen = first_seen
+    if last_seen:
+      self.last_seen = last_seen
+    else:
+      self.last_seen = first_seen
 
   def __repr__(self):
     return "dn: " + self.domain + " hits:" + str(self.hits) + " fs:" + str(self.first_seen) + " ls:" + str(self.last_seen)
@@ -19,12 +22,19 @@ class FediServer():
   def __str__(self):
     return self.__repr__()
 
-  def push(self, ts):
+  def push_hit(self, ts):
     self.hits += 1
-    if ts < self.first_seen:
+    if self.first_seen > ts:
       self.first_seen = ts
-    if ts > self.last_seen:
+    if self.last_seen < ts:
       self.last_seen = ts
+
+  def combine(self, hits, first_seen, last_seen):
+    self.hits += hits
+    if self.first_seen > first_seen:
+      self.first_seen = first_seen
+    if self.last_seen < last_seen:
+      self.last_seen = last_seen
 
 
 # BEGIN EXECUTION
@@ -33,6 +43,7 @@ ap.add_argument(nargs='?', metavar='file', dest='infile', type=argparse.FileType
                   default=sys.stdin, help='Input file if not using stdin')
 ap.add_argument('-d', '--delimiter', type=str, default=',', dest='delimiter', help='Input delimiter')
 ap.add_argument('-t', '--top', dest='top', type=int, help='Output sorted top talking domains only')
+ap.add_argument('-o', '--output-file', dest='outfile', type=str, help='Consolidated output file to update, overrides stdout')
 args = ap.parse_args()
 
 fedi_servers = {}
@@ -53,16 +64,46 @@ for line in args.infile.read().split('\n'):
 
       if url.hostname:
         if url.hostname in fedi_servers:
-          fedi_servers[url.hostname].push(ts)
+          fedi_servers[url.hostname].push_hit(ts)
         else:
           fedi_servers[url.hostname] = FediServer(url.hostname, ts)
 
-if args.top:
-  servers = [v for k,v in fedi_servers.items()]
-  servers.sort(key=lambda x: x.hits, reverse=True)
-  for ii in range(args.top):
-    if ii < len(servers):
-      print(servers[ii])
+if args.outfile:
+  file_servers = {}
+  fh = open(args.outfile, 'r')
+  for line in fh.read().split('\n'):
+    if len(line) == 0:
+      continue
+    if line[0] == '#':
+      continue
+
+    toks = line.split(',')
+    file_servers[toks[0]] = FediServer(toks[0], toks[1], toks[2])
+    file_servers[toks[0]].hits = int(toks[3])
+
+  for key,server in fedi_servers.items():
+    if key in file_servers:
+      file_servers[key].combine(server.hits, server.first_seen, server.last_seen)
+    else:
+      print("key not found")
+      file_servers[key] = FediServer(key, server.first_seen, server.last_seen)
+      file_servers[key].hits = server.hits
+  fh.close()
+
+  print(repr(file_servers))
+  fh = open(args.outfile, 'w')
+  fh.write("#domain,first_seen,last_seen,hits\n")
+  for key,server in file_servers.items():
+    fh.write(key + "," + str(server.first_seen) + "," + str(server.last_seen) + "," + str(server.hits) + "\n")
+  fh.close()
+
 else:
-  for key, server in fedi_servers.items():
-    print(server)
+  if args.top:
+    servers = [v for k,v in fedi_servers.items()]
+    servers.sort(key=lambda x: x.hits, reverse=True)
+    for ii in range(args.top):
+      if ii < len(servers):
+        print(servers[ii])
+  else:
+    for key, server in fedi_servers.items():
+      print(server)
